9dca7f0b6664eed18630de77435a0427ce927d90
disconaked <nolan.klinke@gmail.com>
2018-08-14 17:21:45 -0400

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsList = [
+    {
+      name: "Ahmed",
+      photo: "ahmed@example.com",
+      scores: [
+      "4",
+      "5",
+      "3",
+      "1",
+      "2",
+      "5",
+      "3",
+      "5",
+      "3",
+      "4"
+      ]
+    }
+  ];
+
+  module.exports = friendsList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+
+
+var friendsList = require("../data/friends");
+
+module.exports = function(app) {
+  
+    app.get("/api/friends", function(res) {
+      res.json(friendsList);
+    });
+  
+    app.post("/api/friends", function(req, res) {
+      
+    var newFriendScores = req.body.scores;
+    var submitScores = [];
+    var bestMatch = 0;
+
+    for(var i=0; i<friendsList.length; i++){
+      var scoresDiff = 0;
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friendsList[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      submitScores.push(scoresDiff);
+    }
+
+    for(var i=0; i<submitScores.length; i++){
+      if(submitScores[i] <= submitScores[bestMatch]){
+        bestMatch = i;
+      }
+    }
+
+    var matchFriend = friendsList[bestMatch];
+    res.json(matchFriend);
+
+    friendsList.push(req.body);
+  });
+};
