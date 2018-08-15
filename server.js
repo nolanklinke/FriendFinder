c32ad6b340ce5a7c022704cf758daaacf50fe1dd
disconaked <nolan.klinke@gmail.com>
2018-08-14 20:20:43 -0400

    survey work

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//app.use(bodyParser.text());
-//app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
